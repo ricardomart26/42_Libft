120aa7e4041ba1921696b2e5030d646c023ffd6d
ricardomart26 <ricardomartins2626t@gmail.com>
2021-06-25 01:37:59 +0100

    lib in c

---
+++ ft_lstmap.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstmap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rimartin <rimartin@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/02/12 17:15:28 by rimartin          #+#    #+#             */
+/*   Updated: 2021/02/25 15:23:29 by rimartin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+	t_list *new;
+	t_list *first;
+
+	first = NULL;
+	while (lst)
+	{
+		if (!(new = ft_lstnew(f(lst->content))))
+		{
+			ft_lstclear(&first, del);
+			return (0);
+		}
+		ft_lstadd_back(&first, new);
+		lst = lst->next;
+	}
+	return (first);
+}
